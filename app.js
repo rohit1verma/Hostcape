9758ce7734d5e1229ad9072f2e0cd0658ccd4f7f
rohit1verma <vermarohit7780@gmail.com>
2025-06-17 13:50:56 +0530

    made changes

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -46,7 +45,7 @@ const store = MongoStore.create({
   crypto:{
     secret:process.env.SECRET,
   },
-  touchAfter: 24 * 3600, // time in seconds after which session will be updated
+  touchAfter: 24 * 3600,
 });
 
 store.on("error",() => {
@@ -59,9 +58,9 @@ const sessionOptions = {
   resave : false,
   saveUninitialized : true,
   cookie:{
-    expires : Date.now() + 1000 * 60 * 60 * 24 * 7, // 7 days
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    httpOnly: true, // prevents client-side JavaScript from accessing the cookie
+    expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
   },
 };
 
@@ -82,33 +81,22 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user; // make currentUser available in all templates
+  res.locals.currUser = req.user;
   next();
 });
-
-// app.get("/demouser",async(req,res) => {
-//   let fakeUser = new User({
-//     email:"student@gmail.com",
-//     username:"student",
-   
-//   });
-//   let registeredUser = await User.register(fakeUser,"helloworld");
-//   res.send(registeredUser);
-// })
 
 app.use("/listings", listingRouter);
 app.use("/listings/:listingId/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError("Page Not Found", 404));
-});
+// app.all("*", (req, res, next) => {
+//   next(new ExpressError("Page Not Found", 404));
+// });
 
 app.use((err,req,res,next) => {
   let { statusCode=500, message="Something went wrong" } = err;
   res.status(statusCode).render("error.ejs",{message});
-  // res.status(statusCode).send(message);
-})
+});
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
